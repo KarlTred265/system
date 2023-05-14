9a2fe18f98c89d94c6ab422c9f30fe9573b518e8
KarlTred265 Tred <tred26559@gmail.com>
2023-05-14 15:54:06 +0000

    Deploym

--- booking/settings.py
+++ booking/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["booking.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["system.herokuapp.com", "localhost"]
 
 
 # Application definition
